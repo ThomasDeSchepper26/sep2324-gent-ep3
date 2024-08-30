df6cbc2ac6e5d242e125cced07245436e613bd77
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2024-08-30 12:59:48 +0200

    windows client changes

--- uitvoering/machines/winscripts/winclient/WinclientConf.ps1
+++ uitvoering/machines/winscripts/winclient/WinclientConf.ps1
@@ -1,16 +1,36 @@
-# Adapter naam en gewenste IP-adresinstellingen
-$InterfaceName = "Ethernet"
-$IPAddress = "192.168.108.4"
-$SubnetMask = "24"
-$Gateway = "192.168.108.1"
-$PreferredDNS = "192.168.108.148"
-$AlternateDNS = "8.8.8.8"
+# Administator check
+If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+    Write-Warning "You require admin privileges to run this script, please rerun the script with admin privileges" 
+    Start-Sleep 10
+    Break
+}
 
-New-NetIPAddress -InterfaceAlias $InterfaceName -IPAddress $IPAddress -PrefixLength $SubnetMask
-New-NetRoute -InterfaceAlias $InterfaceName -DestinationPrefix 0.0.0.0/0 -NextHop $Gateway
+# Found a similar function on documentation, changed to match our use case
+function Set-AzertyKeyboard {
+    $validInput = $false
 
-# DNS-instellingen configureren
-Set-DnsClientServerAddress -InterfaceAlias $InterfaceName -ServerAddresses $PreferredDNS, $AlternateDNS
+    while (-not $validInput) {
+        $userInput = Read-Host "Do you want to change the keyboard layout to azerty? (y/n)"
+        
+        switch ($userInput.ToLower()) {
+            "y" {
+                Set-WinUserLanguageList -Force 'en-BE'
+                Write-Host "Azerty keyboard layout enabled" -ForegroundColor Green
+                $validInput = $true
+            }
+            "n" {
+                Write-Host "No changes made to keyboard layout" -ForegroundColor Green
+                $validInput = $true
+            }
+            default {
+                Write-Host "Invalid input. Please enter 'y' to enable Azerty or 'n' to do nothing" -ForegroundColor Red
+            }
+        }
+    }
+}
+
+# Functie call
+Set-AzertyKeyboard
 
 # Installeer de RSAT-functies voor Active Directory, Certificate Services, DHCP, DNS en Group Policy Management
 Add-WindowsCapability -Online -Name Rsat.ActiveDirectory.DS-LDS.Tools~~~~0.0.1.0
@@ -18,16 +38,6 @@ Add-WindowsCapability -Online -Name Rsat.CertificateServices.Tools~~~~0.0.1.0
 Add-WindowsCapability -Online -Name Rsat.DHCP.Tools~~~~0.0.1.0
 Add-WindowsCapability -Online -Name Rsat.Dns.Tools~~~~0.0.1.0
 Add-WindowsCapability -Online -Name Rsat.GroupPolicy.Management.Tools~~~~0.0.1.0
-
-# Uitschakelen van lokale gebruikersaccounts
-$LocalAccounts = Get-WmiObject -Class Win32_UserAccount | Where-Object { $_.LocalAccount -eq $true }
-foreach ($Account in $LocalAccounts) {
-    $AccountName = $Account.Name
-    if ($AccountName -ne "Administrator") {
-        Write-Host "Uitschakelen van lokale gebruikersaccount: $AccountName"
-        net user $AccountName /active:no
-    }
-}
 
 # Instellen van de domeincontroller als het primaire authenticatiemechanisme
 $RegKey = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
@@ -40,6 +50,12 @@ Set-ItemProperty -Path $RegKey -Name "DefaultPassword" -Value ""
 # Join domain
 Add-Computer -DomainName ad.g08-systemsolutions.internal -Credential Administrator -Force
 
-# Toetsenbord instellen
-Set-WinUserLanguageList -LanguageList nl-BE -Force
-Set-WinUILanguageOverride -Language nl-BE
+$action = New-ScheduledTaskAction -Execute 'powershell.exe' -Argument '-NoProfile -ExecutionPolicy Bypass -File "Z:\DisableUsers.ps1"'
+$trigger = New-ScheduledTaskTrigger -AtStartup
+$principal = New-ScheduledTaskPrincipal -UserId "SYSTEM" -LogonType ServiceAccount -RunLevel Highest
+$settings = New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries -DontStopIfGoingOnBatteries -StartWhenAvailable
+Register-ScheduledTask -Action $action -Trigger $trigger -Principal $principal -Settings $settings -TaskName "DisableUsersAtStartup" -Description "Disables all local users at every startup."
+
+Start-Sleep -Seconds 5
+
+Restart-Computer -Force
