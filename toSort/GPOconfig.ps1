c9ff04b6cc6d12e13eaa514ab2ac40f1161fad6b
Jelle Van Holsbeeck <jelle.vanholsbeeck@student.hogent.be>
2024-08-30 08:47:33 +0200

    full uitbreiding CA, ready to be tested

---
+++ toSort/GPOconfig.ps1
@@ -0,0 +1,21 @@
+$GPOConfig = Get-Content -Raw -Path ".\GPOConfig.json" | ConvertFrom-Json
+
+$GPOName = $GPOConfig.GPOName
+$CertFilePath = $GPOConfig.CertFilePath
+$Domain = $GPOConfig.Domain
+$GPOPathTemplate = $GPOConfig.GPOPath
+
+New-GPO -Name $GPOName -Domain $Domain
+New-GPLink -Name $GPOName -Target "DC=$Domain,DC=internal"
+
+$GPO = Get-GPO -Name $GPOName
+$GPOID = $GPO.Id
+$GPOPath = $GPOPathTemplate -replace "{GPOID}", $GPOID
+
+if (-Not (Test-Path -Path $GPOPath)) {
+    New-Item -ItemType Directory -Path $GPOPath -Force
+}
+
+Copy-Item -Path $CertFilePath -Destination $GPOPath
+
+Write-Host "GPO aangemaakt"
